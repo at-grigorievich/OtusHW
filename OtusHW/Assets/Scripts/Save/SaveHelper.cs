a1536ec7e0d60ce82c664a4d2c734272f696f2d8
at-grigorievich-RUDN <at.grigorievich@gmail.com>
2025-04-16 15:25:11 +0300

    add save system

---
+++ OtusHW/Assets/Scripts/Save/SaveHelper.cs
@@ -0,0 +1,38 @@
+﻿using SaveSystem;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using VContainer;
+
+namespace GameEngine.Save
+{
+    public sealed class SaveHelper : MonoBehaviour
+    {
+        [Inject] private ISaveService _saveLoader;
+        [Inject] private IClearable _clearing;
+
+        [Button]
+        public void Save()
+        {
+#if UNITY_EDITOR
+            _saveLoader.Save();
+#endif
+        }
+
+        [Button]
+        public void Load()
+        {
+#if UNITY_EDITOR
+            _saveLoader.Load();
+#endif
+        }
+
+        [Button]
+        public void Clear()
+        {
+#if UNITY_EDITOR
+                _clearing.Clear();
+                Debug.Log("serialize repository cleared");
+#endif
+        }
+    }
+}
